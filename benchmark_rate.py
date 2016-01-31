3b8507d5357d07a9b285703f4dd9d8aabf1bbee2
Andy Sayler <andy.sayler@gmail.com>
2016-01-30 21:55:45 -0700

    refactored script to use better rate limiting

--- benchmark_rate.py
+++ benchmark_rate.py
@@ -32,6 +32,19 @@ def get_token(path_crt, path_key, uid):
     authz = res.json()
     return authz['token']
 
+def get_secret(token, col_uid, sec_uid):
+
+    url = "https://ss.tutamen-test.bdr1.volaticus.net/api/v1/"
+    url += "/collections/" + str(col_uid)
+    url += "/secrets/" + str(sec_uid)
+    url += "/verisons/latest/"
+
+    header = {'tutamen-tokens': token}
+    res = requests.get(url=url, headers=header)
+    res.raise_for_status()
+    authz = res.json()
+    return authz['data']
+
 def get_ac_null_cert(path_crt, path_key):
 
     url = "https://ac.tutamen-test.bdr1.volaticus.net/api/v1/"
@@ -41,6 +54,12 @@ def get_ac_null_cert(path_crt, path_key):
 def get_ac_null():
 
     url = "https://ac.tutamen-test.bdr1.volaticus.net/api/v1/"
+    res = requests.get(url=url)
+    res.raise_for_status()
+
+def get_ss_null():
+
+    url = "https://ss.tutamen-test.bdr1.volaticus.net/api/v1/"
     res = requests.get(url=url)
     res.raise_for_status()
 
@@ -78,71 +97,51 @@ def min_time(sec):
 
     return _decorator
 
+def benchmark(iops_start, iops_end, iops_step, duration, function, *args, **kwargs):
+
+    min_t = 1 #second
+
+    @res_time()
+    def bm_function(*bm_args, **bm_kwargs):
+
+        try:
+            function(*bm_args, **bm_kwargs)
+        except Exception as error:
+            print(error)
+
+    for iops_target in range(iops_start, iops_end, iops_step):
+
+        threads = iops_target * min_t
+        cnt = iops_target * duration
+        pause = 1.0 / iops_target
+
+        futrs = []
+        times = []
+        start = time.time()
+        with futures.ThreadPoolExecutor(max_workers=threads) as e:
+            for i in range(0, cnt):
+                run_t_actual = time.time() - start
+                run_t_target = i * pause
+                if run_t_actual < run_t_target:
+                    time.sleep(run_t_target - run_t_actual)
+                futrs.append(e.submit(bm_function, *args, **kwargs))
+            for f in futrs:
+                times.append(f.result())
+
+        tot = time.time() - start
+        iops = (float(cnt) / float(tot))
+        avg = statistics.mean(times)
+        std = statistics.pstdev(times)
+        print("{}, {:.1f}, {:.1f}, {:.3f}, {:.3f}".format(cnt, tot, iops, avg, std))
+
 if __name__ == "__main__":
 
     path_crt = sys.argv[1]
     path_key = sys.argv[2]
-    iops_s = int(sys.argv[3])
-    iops_f = int(sys.argv[4])
-    step = int(sys.argv[5])
+    iops_start = int(sys.argv[3])
+    iops_end = int(sys.argv[4])
+    iops_step = int(sys.argv[5])
     duration = int(sys.argv[6])
-    min_t = float(sys.argv[7])
 
-    @min_time(min_t)
-    @res_time()
-    def min_get_auth(path_crt, path_key, obj_perm, obj_type, obj_uid=None):
-
-        try:
-            get_auth(path_crt, path_key, obj_perm, obj_type, obj_uid=obj_uid)
-        except Exception as error:
-            print(error)
-
-    @min_time(min_t)
-    @res_time()
-    def min_get_ac_null_cert(path_crt, path_key):
-
-        try:
-            get_ac_null_cert(path_crt, path_key)
-        except Exception as error:
-            print(error)
-
-    @min_time(min_t)
-    @res_time()
-    def min_get_ac_null():
-
-        try:
-            get_ac_null()
-        except Exception as error:
-            print(error)    
-
-    for iops_t in range(iops_s, iops_f, step):
-
-        threads = iops_t * min_t
-        launch = iops_t * duration
-
-
-        futs = []
-        times = []
-        start = time.time()
-        with futures.ThreadPoolExecutor(max_workers=threads) as e:
-#            print("LAUNCHING")
-            for i in range(0, launch):
-                futs.append(e.submit(min_get_ac_null))
-#                futs.append(e.submit(min_get_ac_null_cert, path_crt, path_key))
-#                futs.append(e.submit(min_get_auth, path_crt, path_key, "create", "acserver"))
-#            print("WAITING")
-            for f in futs:
-                times.append(f.result())
-#            print("COMPLETE")
-        length = time.time() - start
-
-        cnt = len(times)
-        avg = statistics.mean(times)
-        std = statistics.stdev(times)
-        iops = (float(cnt) / float(length))
-        print("{}, {:.1f}, {:.3f}, {:.3f}".format(cnt, iops, avg, std))
-
-        # print("Requests         = {}".format(cnt))
-        # print("Response Mean    = {}".format(avg))
-        # print("Response Std Dev = {}".format(std))
-        # print("IOPS Mean        = {}".format(iops))
+    benchmark(iops_start, iops_end, iops_step, duration,
+              get_ac_null_cert, path_crt, path_key)
